f1dec3c972a5bcfb64ad52bd0d882c65b4b12a9d
LeonB <leon@tim-online.nl>
2008-05-15 22:18:29 +0200

    Changed plans: 0.1 => only class methods 0.2 => instance methods too

---
+++ test/tester_class.rb
@@ -0,0 +1,19 @@
+$:.unshift(File.dirname(__FILE__) + '/../lib')
+require 'callbacks'
+
+class Tester
+
+  def boot
+    p "booting"
+  end
+
+  def exit
+    p "exiting"
+  end
+
+  def testing
+    p "testing"
+  end
+
+  include Callbacks
+end
